1361a7e942a9f2b90e92da1bd3c291920cb012d5
DeepSkyWonder <87848500+DeepSkyWonder@users.noreply.github.com>
2021-09-19 16:16:58 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="dsw", 
-    version="0.1.06",
+    version="0.1.07",
     author="Cameron McEwing",
     author_email="tech.mechanic@gmail.com",
     description="Tools used by Deep Sky Wonder",
