fded50e9eeef1ea11422c766e6e32609cabff946
DeepSkyWonder <87848500+DeepSkyWonder@users.noreply.github.com>
2021-09-19 15:49:06 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,7 +13,7 @@ requirements = [
 ]
 
 
-setuptools.setup(
+setup(
     name="dsw", 
     version="0.1.05",
     author="Cameron McEwing",
